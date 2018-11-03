744f6195c298bcacb8981560b88d43085378f26a
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-11-03 16:03:38 +0100

    add dom content loaded event

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { createComponet } from './modules/app.componet';
 import './styles/styles.scss';
 
-// Import all project files to compile;
-
-createComponet().innerHTML = 'Hello TypeScript!';
+// Import all project files to compile
+// TODO ADD FAVICON AND ASSETS TO WEBPACK
+document.addEventListener('DOMContentLoaded', () => {
+  createComponet().innerHTML = 'Hello TypeScript!';
+});
